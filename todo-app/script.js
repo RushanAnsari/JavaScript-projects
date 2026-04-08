8794ce89a68bef1192390e5c2700ce65d8827d13
Rushan Ansari <rushanansari35@gmail.com>
2026-04-08 18:41:20 +0530

    Added Todo App project

---
+++ todo-app/script.js
@@ -0,0 +1,55 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function renderTasks(filter = "all") {
+    const taskList = document.getElementById("taskList");
+    taskList.innerHTML = "";
+
+    tasks.forEach((task, index) => {
+        if (filter === "completed" && !task.completed)return;
+        if(filter === "pending" && task.completed) return;
+
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+        <span onclick="toggleTask(${index})" class="${task.completed ? 'completed' : ''}">
+            ${task.text}
+        </span>
+        <button onclick = "deleteTask(${index})">X</button>
+        `;
+
+        taskList.appendChild(li);
+    });
+}
+
+function addTask(){
+    const input = document.getElementById("taskInput");
+
+    if (input.value.trim() === "") return;
+
+    tasks.push({ text: input.value, completed: false});
+    input.value = "";
+    
+    saveTasks();
+    renderTasks();
+}
+
+function deleteTask(index){
+    tasks.splice(index, 1);
+    saveTasks();
+    renderTasks();
+}
+
+function toggleTask(index) {
+    tasks[index].completed = !tasks[index].completed;
+    saveTasks();
+    renderTasks();
+}
+
+function filterTasks(type) {
+    renderTasks(type);
+}
+renderTasks();
