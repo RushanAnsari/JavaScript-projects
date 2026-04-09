e6bd5e6963c44f3a6e9604a39d4abc47c741d2f6
Rushan Ansari <rushanansari35@gmail.com>
2026-04-09 18:30:48 +0530

    Final portfolio without animation + premium navbar

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,11 +3,3 @@ function scrollToProjects(){
         behavior: "smooth"
     });
 }
-
-document.querySelectorAll('a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href'))
-        .scrollIntoView({behavior: 'smooth'});
-    });
-});
